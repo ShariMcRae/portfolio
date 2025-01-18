6d2ee48c7566893c3f02e3e93546cb1a9fcffdc7
Shari McRae <sharimcrae@protonmail.com>
2025-01-18 14:16:41 -0600

    updated font awesome libraries and changed icons for Titter (X), LinkedIn and GitHub

--- src/components/SocialMediaLinks.jsx
+++ src/components/SocialMediaLinks.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 // Display links to Twitter, LinkedIn and GitHub 
 // profiles using Font Awesome icons.
@@ -10,28 +12,29 @@ export default function SocialMediaLinks() {
   return (
     <div>
       <Link
-        aria-label="Twitter Link"
+        aria-label="X Link"
         to="https://x.com/Shari12376401"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaTwitter size={20} className="me-2" />
-      </Link>
+        
+        <FontAwesomeIcon icon={faXTwitter} />
+      </Link>&nbsp;
       <Link
         aria-label="LinkedIn Link"
         to="https://www.linkedin.com/in/shari-mcrae/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaLinkedin size={20} className="me-2" />
-      </Link>
+        <FontAwesomeIcon icon={faLinkedin} />
+      </Link>&nbsp;
       <Link
         aria-label="GitHub Link"
         to="https://github.com/ShariMcRae"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FaGithub size={20} />
+        <FontAwesomeIcon icon={faGithub} />
       </Link>
     </div>
   );
